a3c00ea2960d1b57fb7b7ed051592a4b3e9b85cc
OktayCopurlu <selimhalim12@gmail.com>
2021-09-29 00:25:23 +0200

    fixed empty input alert

--- app.js
+++ app.js
@@ -1,41 +1,49 @@
 let howManySubmit = document.getElementById("how-many-submit");
-let howManyNumbers = null;
+let getNumber = document.getElementById("how-many");
+let getLabelHowManyNumber = document.getElementById("how-many-number");
 let form = document.getElementById("form");
 let addButton = document.getElementById("input-add");
 let sortButton = document.getElementById("button-sort");
 let printPlace = document.getElementById("print-place");
 let information = document.getElementById("information");
+const userNumber = document.getElementById("input-number");
 let addNumber = 0;
 const numbers = [];
 
 howManySubmit.addEventListener("click", function number() {
-  howManyNumbers = document.getElementById("how-many").valueAsNumber;
-  document.getElementById("how-many").value = "";
-  document.getElementById(
-    "how-many-number"
-  ).innerText = `Add your ${howManyNumbers} number here`;
+  if (getNumber.value === "") {
+    getLabelHowManyNumber.innerText = `You must type a number in first input`;
+  } else {
+    howManyNumbers = getNumber.valueAsNumber;
+    getNumber.value = "";
+    getLabelHowManyNumber.innerText = `Add your ${howManyNumbers} number here`;
+  }
 });
 
 addButton.addEventListener("click", function exercises() {
-  addNumber++;
-  const userNumber = document.getElementById("input-number");
-  if (addNumber <= howManyNumbers) {
-    numbers.push(userNumber.valueAsNumber);
-    userNumber.value = "";
-    information.innerHTML = `You added (${addNumber}) number`;
-  } else if (howManyNumbers === null) {
-    addButton.innerHTML = `Fill first input and click next `;
+  if (userNumber.value === "") {
+    information.innerHTML = `You didn't add any number`;
   } else {
-    addButton.innerHTML = `You can't add any more `;
-    userNumber.value = "";
+    addNumber++;
+    if (addNumber <= howManyNumbers) {
+      numbers.push(userNumber.valueAsNumber);
+      userNumber.value = "";
+      information.innerHTML = `You added (${addNumber}) number`;
+    } else if (howManyNumbers === null) {
+      addButton.innerHTML = `Fill first input and click next `;
+    } else {
+      addButton.innerHTML = `You can't add any more `;
+      userNumber.value = "";
+    }
   }
 });
 
 const sortNumbers = sortButton.addEventListener("click", function sort() {
-  if (numbers.length ===0) {
-  printPlace.innerHTML = `<h3 id="warning">You didn't add any numbers</h3>`;
-    
-  }else{
-  printPlace.innerHTML = `<h3 id="succeed">${numbers.sort((a, b) => a - b)}</h3>`;
-}
+  if (numbers.length === 0) {
+    printPlace.innerHTML = `<h3 id="warning">You didn't add any numbers</h3>`;
+  } else {
+    printPlace.innerHTML = `<h3 id="succeed">${numbers.sort(
+      (a, b) => a - b
+    )}</h3>`;
+  }
 });
